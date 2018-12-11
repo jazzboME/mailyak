c6f9b0dc0ccf61fd1c2a757857a6ad4b55ed91e6
Kevin Foss <kfoss@maine.edu>
2018-12-11 17:20:00 -0500

    Initial attempt at new SendMail()

--- mailyak.go
+++ mailyak.go
@@ -75,6 +75,71 @@ func (m *MailYak) Send() error {
 	)
 }
 
+// SendMail attempts to provide a more featureful version of the Send command
+//
+// Goal is to allow low level adjustments, such as setting the HELO response
+func (m *MailYak) SendMail(addr string, a smtp.Auth, from string, to []string, msg []byte) error {
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	for _, recp := range to {
+		if err := validateLine(recp); err != nil {
+			return err
+		}
+	}
+	c, err := smtp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err := c.Hello("UMF-FW1.um.maine.edu"); err != nil {
+		return err
+	}
+	
+	//if err = c.hello(); err != nil {
+	//	return err
+	//}
+
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		config := &tls.Config{ServerName: c.serverName}
+		if testHookStartTLS != nil {
+			testHookStartTLS(config)
+		}
+		if err = c.StartTLS(config); err != nil {
+			return err
+		}
+	}
+	if a != nil && c.ext != nil {
+		if _, ok := c.ext["AUTH"]; !ok {
+			return errors.New("smtp: server doesn't support AUTH")
+		}
+		if err = c.Auth(a); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
+
 // MimeBuf returns the buffer containing all the RAW MIME data.
 //
 // MimeBuf is typically used with an API service such as Amazon SES that does
@@ -134,4 +199,12 @@ func (m *MailYak) HTML() *BodyPart {
 // Plain returns a BodyPart for the plain-text email body.
 func (m *MailYak) Plain() *BodyPart {
 	return &m.plain
+}
+
+// validateLine checks to see if a line has CR or LF as per RFC 5321
+func validateLine(line string) error {
+	if strings.ContainsAny(line, "\n\r") {
+		return errors.New("smtp: A line must not contain CR or LF")
+	}
+	return nil
 }
